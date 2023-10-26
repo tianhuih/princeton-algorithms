7572b7dc3320b6205d84855dfb7d1bbe34666f7f
Tianhui Huang <t.tianhui.huang@gmail.com>
2023-10-25 19:03:54 -0700

    implemented weighted quick union

--- union-find/WeightedQuickUnionPathCompression.java
+++ union-find/WeightedQuickUnionPathCompression.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class WeightedQuickUnionUF {
+public class WeightedQuickUnionPathCompression {
 
     private int[] parents;
-    private int[] size;     // size[i] = number of nodes of tree rooted in i
+    private int[] size;
     private int count;
 
-    public WeightedQuickUnionUF(int n) {
+    public WeightedQuickUnionPathCompression(int n) {
         parents = new int[n];
         size = new int[n];
         count = n;
@@ -22,16 +22,31 @@ public class WeightedQuickUnionUF {
         int proot = find(p);
         int qroot = find(q);
         if (proot == qroot) return;
-        parents[proot] = qroot;
+
+        // connect the smaller tree to the larger tree
+        if (size[proot] >= size[qroot]) {
+            parents[qroot] = proot;
+            size[proot] += size[qroot];
+        } else {
+            parents[proot] = qroot;
+            size[qroot] += size[proot];
+        }
         count--;
     }
 
+
     public int find(int p) {
         validate(p);
-        while (parents[p] != p) {
-            p = parents[p];
+        int root = p;
+        while (parents[root] != root) {
+            root = parents[root];
         }
-        return p;
+        while (parents[p] != p) {
+            int next = parents[p];
+            parents[p] = root;
+            p = next;
+        }
+        return root;
     }
 
     public boolean isConnected(int p, int q) {
@@ -44,6 +59,12 @@ public class WeightedQuickUnionUF {
         return count;
     }
 
+
+    public int size(int p) {
+        validate(p);
+        return size[p];
+    }
+
     private void validate(int p) {
         int n = parents.length;
         if (p < 0 || p >= n) {
@@ -54,7 +75,7 @@ public class WeightedQuickUnionUF {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        WeightedQuickUnionPathCompression uf = new WeightedQuickUnionPathCompression(n);
         System.out.println("initialized UF with size " + n);
         sc.nextLine();
 
@@ -79,6 +100,9 @@ public class WeightedQuickUnionUF {
                 case "count":
                     System.out.println(uf.count());
                     break;
+                case "size":
+                    System.out.println(uf.size(Integer.parseInt(commands[1])));
+                    break;
                 case "exit":
                     return;
                 default:
@@ -102,6 +126,8 @@ public class WeightedQuickUnionUF {
             case "count":
                 validateIntegers(commands, 0);
                 break;
+            case "size":
+                validateIntegers(commands, 1);
             case "exit":
                 break;
             default:
